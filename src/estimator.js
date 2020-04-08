2814266ee0600b56dade27cd7265caf678321555
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-09 00:07:18 +0100

    estimator infectionsByRequestedTime returned value is now dependent on the time & period type in input

--- src/estimator.js
+++ src/estimator.js
@@ -8,10 +8,31 @@ const covid19ImpactEstimator = (data) => {
   result.impact.currentlyInfected = data.reportedCases * 10;
   result.severeImpact.currentlyInfected = data.reportedCases * 50;
 
-  result.impact.infectionsByRequestedTime = result.impact.currentlyInfected * 512;
-  result.severeImpact.infectionsByRequestedTime = result.severeImpact.currentlyInfected * 512;
+  const factor = getInfectionsFactor(data.periodType, data.timeToElapse);
+
+  result.impact.infectionsByRequestedTime = result.impact.currentlyInfected * (2 ** factor);
+  result.severeImpact.infectionsByRequestedTime = result.severeImpact.currentlyInfected * (2 ** factor);
 
   return result;
+};
+
+const getInfectionsFactor = (periodType, timeToElapse) => {
+  let factor;
+  switch (periodType) {
+    case 'days':
+      factor = Math.floor(timeToElapse / 3);
+      break;
+    case 'weeks':
+      factor = Math.floor(timeToElapse * 7 / 3);
+      break;
+    case 'months':
+      factor = Math.floor(timeToElapse * 30 / 3);
+      break;
+    default:
+      factor = 1;
+      break;
+  }
+  return factor;
 };
 
 export default covid19ImpactEstimator;
--- src/estimator.test.js
+++ src/estimator.test.js
@@ -8,7 +8,7 @@ const data = {
     avgDailyIncomePopulation: 0.71
   },
   periodType: 'days',
-  timeToElapse: 58,
+  timeToElapse: 28,
   reportedCases: 674,
   population: 66622705,
   totalHospitalBeds: 1380614
