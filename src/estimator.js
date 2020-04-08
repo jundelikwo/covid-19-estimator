6ac1ee895a83e91fc38af8237538ba8e5570388b
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-09 00:21:13 +0100

    Added code to pass the 'impact.severeCasesByRequestedTime returns the correct data' test

--- src/estimator.js
+++ src/estimator.js
@@ -1,21 +1,3 @@
-const covid19ImpactEstimator = (data) => {
-  const result = {
-    data,
-    impact: {},
-    severeImpact: {}
-  };
-
-  result.impact.currentlyInfected = data.reportedCases * 10;
-  result.severeImpact.currentlyInfected = data.reportedCases * 50;
-
-  const factor = getInfectionsFactor(data.periodType, data.timeToElapse);
-
-  result.impact.infectionsByRequestedTime = result.impact.currentlyInfected * (2 ** factor);
-  result.severeImpact.infectionsByRequestedTime = result.severeImpact.currentlyInfected * (2 ** factor);
-
-  return result;
-};
-
 const getInfectionsFactor = (periodType, timeToElapse) => {
   let factor;
   switch (periodType) {
@@ -33,6 +15,26 @@ const getInfectionsFactor = (periodType, timeToElapse) => {
       break;
   }
   return factor;
+};
+
+const covid19ImpactEstimator = (data) => {
+  const result = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+
+  result.impact.currentlyInfected = data.reportedCases * 10;
+  result.severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  const factor = getInfectionsFactor(data.periodType, data.timeToElapse);
+
+  result.impact.infectionsByRequestedTime = result.impact.currentlyInfected * (2 ** factor);
+  result.severeImpact.infectionsByRequestedTime = result.severeImpact.currentlyInfected * (2 ** factor);
+
+  result.impact.severeCasesByRequestedTime = result.impact.infectionsByRequestedTime * 0.15;
+
+  return result;
 };
 
 export default covid19ImpactEstimator;
