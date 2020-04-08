b6b25d82910068a23b66cd3aa1db0d65b17b4b58
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-08 23:35:08 +0100

    Added test to check if the estimator returns the right data structure

---
+++ src/estimator.test.js
@@ -0,0 +1,27 @@
+import covid19ImpactEstimator from './estimator';
+
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+
+test('returns the right data structure', () => {
+  const result = covid19ImpactEstimator(data);
+  expect(result).toEqual(
+    expect.objectContaining({
+      data: expect.any(Object),
+      impact: expect.any(Object),
+      severeImpact: expect.any(Object)
+    })
+  );
+  // expect((123)).toBe(123);
+});
