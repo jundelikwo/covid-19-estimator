370f4f6c976ddde5ec73ef99813176b67a6f3ad0
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-08 23:52:09 +0100

    Added code to pass the 'impact.infectionsByRequestedTime returns the correct data' test

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,16 @@
-const covid19ImpactEstimator = (data) => ({
-  data,
-  impact: {
-    currentlyInfected: data.reportedCases * 10
-  },
-  severeImpact: {
-    currentlyInfected: data.reportedCases * 50
-  }
-});
+const covid19ImpactEstimator = (data) => {
+  const result = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+
+  result.impact.currentlyInfected = data.reportedCases * 10;
+  result.severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  result.impact.infectionsByRequestedTime = result.impact.currentlyInfected * 512;
+
+  return result;
+};
 
 export default covid19ImpactEstimator;
