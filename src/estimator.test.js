a02838177bbf065dab5ba00469b8f1af8bb92177
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-13 00:00:48 +0100

    Updated dollarsInFlight test to check for whole numbers

--- src/estimator.test.js
+++ src/estimator.test.js
@@ -153,11 +153,11 @@ test('severeImpact.casesForVentilatorsByRequestedTime returns the correct data',
 test('impact.dollarsInFlight returns the correct data', () => {
   const result = covid19ImpactEstimator(data);
   expect(result.impact.dollarsInFlight)
-    .toBe((data.reportedCases * 10 * 524288 * 0.71 * 5 * 58).toFixed(2));
+    .toBe(Math.floor(data.reportedCases * 10 * 524288 * 0.71 * 5 * 58));
 });
 
 test('severeImpact.dollarsInFlight returns the correct data', () => {
   const result = covid19ImpactEstimator(data);
   expect(result.severeImpact.dollarsInFlight)
-    .toBe((data.reportedCases * 50 * 524288 * 0.71 * 5 * 58).toFixed(2));
+    .toBe(Math.floor(data.reportedCases * 50 * 524288 * 0.71 * 5 * 58));
 });
