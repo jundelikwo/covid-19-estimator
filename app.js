df2ec64d1fd5b705715090156f4e988311daf449
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2020-04-13 00:44:39 +0100

    Added logging functionality to /api/v1/on-covid-19 api route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,14 +6,23 @@ const estimator = require('./src/estimator');
 
 const app = express();
 
+const logFile = 'logs.txt';
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use(cors());
 
 app.post('/api/v1/on-covid-19', (req, res) => {
-  res.json(estimator(req.body));
+  const start = new Date();
+  const result = estimator(req.body);
+  const end = new Date();
+
+  fs.appendFile(logFile, `POST \t ${req.route.path} \t 200 \t ${end - start}ms \n`, () => {});
+  
+  res.json(result);
 });
+
 app.listen(3000);
 
 console.log('Express app running on port 3000');
